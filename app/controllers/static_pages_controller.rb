5adc23eae9ba2bdb8d0f62d0e23c68a65206a66d
David Moss <precedex@gmail.com>
2013-11-24 02:55:28 -0500

    basic pages up

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,14 @@
 class StaticPagesController < ApplicationController
   def welcome
   end
+  
+  def feedback
+  end
+  
+  def resources
+  end
+  
+  def search
+  end
+  
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,8 +5,32 @@ describe "Static Pages" do
   describe "Welcome page" do
     
     it "should have the content 'Welcome'" do
-      visit '/static_pages/welcome'
+      visit root_path
       expect(page).to have_content('Welcome')
     end
   end
+    
+    describe "Feedback page" do
+    
+      it "should have the content 'Feedback'" do
+        visit feedback_path
+        expect(page).to have_content('Feedback')
+      end   
+    end
+    
+    describe "Resources page" do
+    
+      it "should have the content 'Resources'" do
+        visit feedback_path
+        expect(page).to have_content('Resources')
+      end   
+    end
+    
+    describe "Search page" do
+    
+      it "should have the content 'Search'" do
+        visit feedback_path
+        expect(page).to have_content('Search')
+      end   
+    end
 end
