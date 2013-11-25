37b3902d7c05a78e269afdf62ddd5f59c18c5b6d
David Moss <precedex@gmail.com>
2013-11-24 19:42:39 -0500

    3 textbooks, some reformat

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 Resource.delete_all
 
-Resource.create!(title: 'Basics of Anesthesia, 6th ed.',
+Resource.create!(title: 'Basics of Anesthesia, 6th ed. (2011)',
                  authors: "Ronald D. Miller, Manuel C. Pardo, Jr.",
                  description: "everyone and their mother read this book.",
                  image_url: 'baby_miller.jpg',
                  link: "http://www.amazon.com/Basics-Anesthesia-Expert-Consult-Online/dp/1437716148/ref=sr_1_1?ie=UTF8&qid=1385336065&sr=8-1&keywords=basics+of+anesthesia")
                  
-Resource.create!(title: 'Morgan & Mikahil\'s Clinical Anesthesiology, 5th ed.',
+Resource.create!(title: 'Morgan & Mikhail\'s Clinical Anesthesiology, 5th ed. (2013)',
                  authors: "John F. Butterworth, David C. Mackey, John D. Wasnick",
                  description: "everyone and their sister read this book.",
                  image_url: 'morgan_mikhail.jpg',
                  link: "http://www.amazon.com/Morgan-Mikhails-Clinical-Anesthesiology-Medical/dp/0071627030/ref=sr_1_1?ie=UTF8&qid=1385336644&sr=8-1&keywords=morgan+mikhail")
                 
-Resource.create!(title: 'Clinical Cases in Anesthesia, 4th ed.',
+Resource.create!(title: 'Clinical Cases in Anesthesia, 4th ed. (2014)',
                  authors: "Allen P. Reed, Francine S. Yudkowitz",
                  description: "everyone and their brother read this book.",
                  image_url: 'clinical_cases.jpg',
