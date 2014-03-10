98bf23ac62d809201f6dee1f319311e8accbe611
Jacob Maine <jacob.maine@gmail.com>
2014-03-10 17:18:22 +0300

    Hashes are nicer than repeated ifs

--- app/helpers/sites_helper.rb
+++ app/helpers/sites_helper.rb
@@ -9,4 +9,22 @@ module SitesHelper
   def link_to_sdn_forum
     external_link_to image_tag("SDN2.jpg"), "http://forums.studentdoctor.net/forums/anesthesiology.45/"
   end
+
+  CATEGORY_TITLES = {
+    "education" => "Miscellaneous",
+    "education-regional" => "Regional Anesthesia",
+    "education-video" => "Videos",
+    "education-simulation" => "Simulation",
+    "journal" => "Journals",
+    "blog" => "Blogs",
+    "society-us" => "US Anesthesiology Societies",
+    "society-state" => "State Anesthesiology Societies",
+    "society-intl" => "Intl. Anesthesiology Societies",
+    "society-pain" => "Regional Anesthesia & Pain Societies",
+    "society-crna" => "Nurse Anesthesia Societies"
+  }
+
+  def site_category_title(category)
+    CATEGORY_TITLES[category]
+  end
 end
