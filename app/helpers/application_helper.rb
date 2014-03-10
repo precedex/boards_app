0de144c9dee02110afe5a238e8da9eebbc1c2d9e
Jacob Maine <jacob.maine@gmail.com>
2014-03-10 17:32:29 +0300

    DRY up validation errors in HTML

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,17 @@ module ApplicationHelper
   def external_link_to(name = nil, options = nil, html_options = nil)
     link_to(name, options, (html_options || {}).merge(target: "_blank"))
   end
+
+  def old_style_error_div(model)
+    return unless model.errors.any?
+
+    summary = "#{pluralize(model.errors.count, 'error')} prohibited this #{model.class.name.downcase} from being saved:"
+    error_items = model.errors.full_messages.map do |msg|
+      content_tag(:li, msg)
+    end.join(" ").html_safe
+
+    content_tag(:div,
+                content_tag(:h2, summary) + content_tag(:ul, error_items),
+                id: "error_explanation")
+  end
 end
