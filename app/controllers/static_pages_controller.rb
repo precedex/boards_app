b9e7c30fdd71ef7ce5beaaa8d193060341cbab85
Jacob Maine <jacob.maine@gmail.com>
2014-03-10 15:02:51 +0300

    DRY up oral boards static page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,24 +1,61 @@
 class StaticPagesController < ApplicationController
   def welcome
   end
-  
+
   def feedback
   end
-  
+
   def resources
   end
-  
+
   def about
   end
-  
+
   def search
   end
-  
+
   def tools
   end
-  
+
   def secret
   end
-  
-  
+
+  def orals
+    @aba_archive = ["ABA", 'http://theaba.docstream.com/videos/archive/?page=1']
+    @exam_hints_pdf = ["PDF", 'http://anesthesiaboardhints.homestead.com/HintsfortheAnesthesiologyBoardExaminationRev1.6.pdf']
+    @the_anesthesia_consultant = ["The Anesthesia Consultant", 'http://theanesthesiaconsultant.com/2013/08/22/advice-for-passing-the-oral-board-exams-in-anesthesiology/']
+    @the_student_doctor_network = [view_context.image_tag("SDN2.jpg"), "http://forums.studentdoctor.net/forums/anesthesiology.45/"]
+
+    @study_strategy = [
+      ["Which Anesthesia Oral Board Review Course? (1/14)",                   "http://forums.studentdoctor.net/threads/which-anesthesia-oral-board-review-course.1052755/"],
+      ["2014 Oral Boards Study Plan (1/14)",                                  "http://forums.studentdoctor.net/threads/2014-oral-boards-study-plan.1048582/"],
+      ["Oral Board Studying Tips (5/13)",                                     "http://forums.studentdoctor.net/threads/oral-board-studying-tips.1004846/"],
+      ["Oral Boards: the Ho Course (12/12)",                                  "http://forums.studentdoctor.net/threads/oral-boards-the-ho-course.973389/"],
+      ["Oral Boards for Dummies (10/12)",                                     "http://forums.studentdoctor.net/threads/oral-boards-for-dummies.960908/"],
+      ["Just took oral board exam: Holy ... (10/12)",                         "http://forums.studentdoctor.net/threads/just-took-oral-board-exam-holy-amp.954763/"],
+      ["Oral Board Course- Which One? (9/12)",                                "http://forums.studentdoctor.net/threads/oral-board-course-which-one-here-are-some-tips.952284/"],
+      ["Fall Orals (8/12)",                                                   "http://forums.studentdoctor.net/threads/fall-orals.942088/"],
+      ["I failed the oral boards again (5/12)",                               "http://forums.studentdoctor.net/threads/i-failed-the-oral-boards-again.914746/"],
+      ["April ABA Oral Exam (3/12)",                                          "http://forums.studentdoctor.net/threads/april-aba-oral-exam.897213/"],
+      ["Oral Board Prep Course (1/12)",                                       "http://forums.studentdoctor.net/threads/oral-board-prep-course.883011/"],
+      ["Just Oral Boards (11/11)",                                            "http://forums.studentdoctor.net/threads/just-oral-boards.868158/"],
+      ["Oral Boards: Self-Study vs Review Course (10/11)",                    "http://forums.studentdoctor.net/threads/oral-boards-self-study-vs-review-course.863807/"],
+      ["Oral advice (10/11)",                                                 "http://forums.studentdoctor.net/threads/oral-advice.863646/"],
+      ["Oral Board Book (10/11)",                                             "http://forums.studentdoctor.net/threads/oral-board-book.863401/"],
+      ["Best Oral Boards Resource - not going to a session (7/11)",           "http://forums.studentdoctor.net/threads/best-oral-boards-resource-not-going-to-a-session.842324/"],
+      ["My Oral Board Experience (5/11)",                                     "http://forums.studentdoctor.net/threads/my-oral-board-experience.822916/"],
+      ["What is the best way to study for the anesthesia oral boards (5/11)", "http://forums.studentdoctor.net/threads/what-is-the-best-way-to-study-for-the-anesthesia-oral-boards.821337/"],
+    ]
+
+    @commercial_courses = [
+      ["Board Stiff Live",                             "https://boardstifflive.com"],
+      ["Dr. Jensen Best Medicine for Oral Boards",     "http://www.anesthesiologyboards.com/oral/"],
+      ["GadsenJones Anesthesiology Oral Board Review", "http://www.gadsdenjones.com"],
+      ["Just Oral Boards",                             "http://www.justoralboards.com"],
+      ["Nail the Boards",                              "http://www.nailtheboards.com"],
+      ["Michael Ho: The Essential Oral Board Review",  "http://www.anconsultants.com"],
+      ["The Osler Institute",                          "http://www.osler.org/main/ane.html"],
+      ["Ultimate Board Prep",                          "http://www.ultimateboardprep.com"]
+    ]
+  end
 end
--- app/helpers/sites_helper.rb
+++ app/helpers/sites_helper.rb
@@ -1,2 +1,8 @@
 module SitesHelper
+  def site_list(sites)
+    list_items = sites.map do |content, link|
+      content_tag(:li, external_link_to(content, link))
+    end.join.html_safe
+    content_tag(:ul, list_items)
+  end
 end
