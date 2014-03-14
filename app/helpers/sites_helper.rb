0816dfe2844c08fd1594c933409c6121eac1e252
Jacob Maine <jacob.maine@gmail.com>
2014-03-14 23:41:33 +0300

    Convert another helper to a partial

--- app/helpers/sites_helper.rb
+++ app/helpers/sites_helper.rb
@@ -1,11 +1,4 @@
 module SitesHelper
-  def site_list(sites)
-    list_items = sites.map do |content, link|
-      content_tag(:li, external_link_to(content, link))
-    end.join.html_safe
-    content_tag(:ul, list_items)
-  end
-
   def link_to_sdn_forum
     external_link_to image_tag("SDN2.jpg"), "http://forums.studentdoctor.net/forums/anesthesiology.45/"
   end
