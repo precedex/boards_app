9cf26a4b4e45d2475460930da3f9794f93b6e078
Jacob Maine <jacob.maine@gmail.com>
2014-03-15 13:11:54 +0300

    Reorganize and document routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,41 +1,7 @@
 BoardsApp::Application.routes.draw do
 
-  root  'static_pages#welcome'
-
-  get   '/sites/blogs',                   to: 'sites#index',     :category => 'blog'
-
-  get   '/sites/education',               to: 'sites#index',     :category => 'education'
-  get   '/sites/education/regional',      to: 'sites#index',     :category => 'education-regional'
-  get   '/sites/education/video',         to: 'sites#index',     :category => 'education-video'
-  get   '/sites/education/simulation',    to: 'sites#index',     :category => 'education-simulation'
-
-  get   '/sites/journals',                to: 'sites#index',     :category => 'journal'
-
-  get   '/sites/societies/us',            to: 'sites#index',     :category => 'society-us'
-  get   '/sites/societies/state',         to: 'sites#index',     :category => 'society-state'
-  get   '/sites/societies/intl',          to: 'sites#index',     :category => 'society-intl'
-  get   '/sites/societies/pain',          to: 'sites#index',     :category => 'society-pain'
-  get   '/sites/societies/crna',          to: 'sites#index',     :category => 'society-crna'
-
-  get    '/secret',                       to: 'static_pages#secret'
-  get    '/questions/random',             to: 'questions#random'
-  get    '/feedback',                     to: 'static_pages#feedback'
-  get    '/tools',                        to: 'static_pages#tools'
-  get    '/about',                        to: 'static_pages#about'
-  get    '/sites/writtenboards',          to: 'static_pages#writtens'
-  get    '/sites/oralboards',             to: 'static_pages#orals'
-  get    '/sites/pedsboards',             to: 'static_pages#pedsboards'
-
-  get    '/sites',                        to: 'sites#index'
-  get    '/orals',                        to: 'static_pages#orals'
-  get    '/check_answer/:id',             to: 'questions#check_answer'
-
-  resources :questions, :sites, :resources
-
-  resources :categories, only: [] do
-    resources :resources, only: [:index]
-  end
-
+  # Deprecated routes
+  # At the top, because they might otherwise match a real route
   get   '/resources/reference_textbooks', to: redirect("/categories/1/resources")
   get   '/resources/concise_textbooks',   to: redirect("/categories/2/resources")
   get   '/resources/case-based_textbooks',to: redirect("/categories/3/resources")
@@ -45,4 +11,50 @@ BoardsApp::Application.routes.draw do
   get   '/resources/handbooks',           to: redirect("/categories/7/resources")
   get   '/resources/review_textbooks',    to: redirect("/categories/8/resources")
   get   '/resources/oralboards',          to: redirect("/categories/9/resources")
+
+  get    '/orals',                        to: redirect("/sites/oralboards")
+
+  # home page
+  root  'static_pages#welcome'
+
+  # blogs
+  get   '/sites/blogs',                   to: 'sites#index',     :category => 'blog'
+
+  # education
+  get   '/sites/education',               to: 'sites#index',     :category => 'education'
+  get   '/sites/education/regional',      to: 'sites#index',     :category => 'education-regional'
+  get   '/sites/education/video',         to: 'sites#index',     :category => 'education-video'
+  get   '/sites/education/simulation',    to: 'sites#index',     :category => 'education-simulation'
+
+  # journals
+  get   '/sites/journals',                to: 'sites#index',     :category => 'journal'
+
+  # societies
+  get   '/sites/societies/us',            to: 'sites#index',     :category => 'society-us'
+  get   '/sites/societies/state',         to: 'sites#index',     :category => 'society-state'
+  get   '/sites/societies/intl',          to: 'sites#index',     :category => 'society-intl'
+  get   '/sites/societies/pain',          to: 'sites#index',     :category => 'society-pain'
+  get   '/sites/societies/crna',          to: 'sites#index',     :category => 'society-crna'
+
+  # tools
+  get    '/tools',                        to: 'static_pages#tools'
+
+  # exams
+  get    '/sites/writtenboards',          to: 'static_pages#writtens'
+  get    '/sites/oralboards',             to: 'static_pages#orals'
+  get    '/sites/pedsboards',             to: 'static_pages#pedsboards'
+
+  # footer
+  get    '/secret',                       to: 'static_pages#secret'
+  get    '/feedback',                     to: 'static_pages#feedback'
+  get    '/about',                        to: 'static_pages#about'
+
+  get    '/questions/random',             to: 'questions#random'
+  get    '/check_answer/:id',             to: 'questions#check_answer'
+
+  resources :questions, :sites, :resources
+
+  resources :categories, only: [] do
+    resources :resources, only: [:index]
+  end
 end
