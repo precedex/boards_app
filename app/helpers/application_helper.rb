b97ffb8fc93388c660ffdfd8aebf5df73d64e94f
Jacob Maine <jacob.maine@gmail.com>
2014-03-15 16:37:55 +0300

    Move helpers to appropriate places

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -24,4 +24,38 @@ module ApplicationHelper
                 content_tag(:h2, summary) + content_tag(:ul, error_items),
                 id: "error_explanation")
   end
+
+  def on_books_path?
+    current_page?(category_resources_path(1)) ||
+    current_page?(category_resources_path(2)) ||
+    current_page?(category_resources_path(3)) ||
+    current_page?(category_resources_path(4)) ||
+    current_page?(category_resources_path(5)) ||
+    current_page?(category_resources_path(6)) ||
+    current_page?(category_resources_path(7)) ||
+    current_page?(category_resources_path(8)) ||
+    current_page?(category_resources_path(9)) ||
+    current_page?(resources_path)
+  end
+
+  def on_exams_path?
+    current_page?(sites_writtenboards_path) ||
+    current_page?(sites_oralboards_path) ||
+    current_page?(sites_pedsboards_path)
+  end
+
+  def on_education_path?
+    current_page?(sites_education_regional_path) ||
+    current_page?(sites_education_video_path) ||
+    current_page?(sites_education_simulation_path) ||
+    current_page?(sites_education_path)
+  end
+
+  def on_societies_path?
+    current_page?(sites_societies_us_path) ||
+    current_page?(sites_societies_state_path) ||
+    current_page?(sites_societies_intl_path) ||
+    current_page?(sites_societies_crna_path) ||
+    current_page?(sites_societies_pain_path)
+  end
 end
