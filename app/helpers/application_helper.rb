b930075fff32c8b3d393975179f0c1ac0222d994
DavidMoss <precedex@gmail.com>
2014-12-22 17:03:22 -0500

    improve lectures format railscast 228

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -24,6 +24,13 @@ module ApplicationHelper
                 content_tag(:h2, summary) + content_tag(:ul, error_items),
                 id: "error_explanation")
   end
+  
+  def sortable(column, title = nil)
+    title ||= column.titleize
+    css_class = column == sort_column ? "current #{sort_direction}" : nil
+    direction = column == sort_column && sort_direction == 'asc' ? 'desc' : 'asc'
+    link_to title, {:sort => column, :direction => direction}, {:class => css_class} 
+  end
 
   def on_books_path?
     current_page?(category_resources_path(1)) ||
