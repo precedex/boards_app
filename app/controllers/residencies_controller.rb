83c7ce7e7d2f99e9f514aaaa6c24a58d9e88b2c2
DavidMoss <precedex@gmail.com>
2015-09-01 17:15:55 -0400

    residency update

--- app/controllers/residencies_controller.rb
+++ app/controllers/residencies_controller.rb
@@ -1,5 +1,13 @@
 class ResidenciesController < ApplicationController
   def index
+    
+    
+    @acgme = "https://www.acgme.org/acgmeweb/"
+    @acgmeanes = "https://www.acgme.org/acgmeweb/tabid/128/ProgramandInstitutionalAccreditation/Hospital-BasedSpecialties/Anesthesiology.aspx"
+    @res = "https://www.acgme.org/acgmeweb/Portals/0/PFAssets/ProgramRequirements/040_anesthesiology_2016.pdf"
+    @milestone = "https://www.acgme.org/acgmeweb/Portals/0/PDFs/Milestones/AnesthesiologyMilestones.pdf"
+    @aba = "http://www.theaba.org/"
+    
     @residencies = Residency.all.by_state
   end
 
