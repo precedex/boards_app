f64b9d5038673a20a05cf39897af7a2e8cf7ab12
DavidMoss <precedex@gmail.com>
2016-09-09 11:13:55 -0400

    informatics search

--- app/controllers/informatics_controller.rb
+++ app/controllers/informatics_controller.rb
@@ -7,9 +7,8 @@ class InformaticsController < ApplicationController
     @subsubheadings = Subsubheading.all
     @subsubsubheadings = Subsubsubheading.all
      
-    @informatics = Informatic.all(:conditions => "section like '#{params[:section_id]}%'")
-    @informatics = @informatics.sort! { |a,b| a.section <=> b.section }
-    #@informatics = Informatic.search(params[:search])
+    
+    
     
   end
   
