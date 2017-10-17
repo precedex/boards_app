603289b51c3dfbc25614ba28efbfd86b34541b62
DavidMoss <precedex@gmail.com>
2017-10-17 16:29:31 -0400

    adjustments

---
+++ Update.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+bundle exec rake assets:precompile
+git status
+git add .
+git commit -m "adjustments"
+git push
+git push heroku master
+heroku pg:reset HEROKU_POSTGRESQL_MAROON --confirm anesthesiahub
+heroku pg:push pg_database_development HEROKU_POSTGRESQL_MAROON
