af75bac137b79f2124951c3904dae4b729e97310
DavidMoss <precedex@gmail.com>
2019-03-29 10:52:52 -0400

    adjustments

--- Update.sh
+++ Update.sh
@@ -4,6 +4,6 @@ git status
 git add .
 git commit -m "adjustments"
 git push
-git push heroku master
+git push heroku master --app anesthesiahub
 heroku pg:reset HEROKU_POSTGRESQL_MAROON --confirm anesthesiahub
 heroku pg:push pg_database_development HEROKU_POSTGRESQL_MAROON
