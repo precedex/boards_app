d5768cb4542ae7ff4df62163f3787448d90e5db7
David Moss <precedex@gmail.com>
2020-06-20 10:20:04 -0400

    more changes to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,14 +20,14 @@ BoardsApp::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = true
+  config.serve_static_files = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
   config.public_file_server.enabled = true
 
   # Generate digests for assets URLs.
@@ -61,6 +61,7 @@ BoardsApp::Application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile = ['*.js', '*.css', '*.css.erb']
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
