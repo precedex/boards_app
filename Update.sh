aed9c652fa1de421e76f62f5d39101c9da2aaf49
David Moss <precedex@gmail.com>
2021-10-28 12:48:56 -0400

    adjustments

--- Update.sh
+++ Update.sh
@@ -3,7 +3,10 @@ bundle exec rake assets:precompile
 git status
 git add .
 git commit -m "adjustments"
-git push
+git push https://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@github.com/precedex/boards_app.git
 git push heroku master
+
 heroku pg:reset HEROKU_POSTGRESQL_MAROON --confirm anesthesiahub --app anesthesiahub
 heroku pg:push pg_database_development HEROKU_POSTGRESQL_MAROON --app anesthesiahub
+
+
